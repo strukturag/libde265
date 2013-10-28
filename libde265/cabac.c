fa6b203c5c411a77fd98c37ede3033340e3731f5
Dirk Farin <dirk.farin@gmail.com>
2013-10-28 19:06:06 +0100

    remove CABAC logging output

--- libde265/cabac.c
+++ libde265/cabac.c
@@ -160,10 +160,7 @@ void init_CABAC_decoder(CABAC_decoder* decoder, uint8_t* bitstream, int length)
 
 void init_CABAC_decoder_2(CABAC_decoder* decoder)
 {
-  fprintf(stderr,"RESETTING CABAC at byte offset %d\n",decoder->bitstream_curr - decoder->bitstream_start);
-
   int length = decoder->bitstream_end - decoder->bitstream_curr;
-  fprintf(stderr,"LENGTH: %d\n",length);
 
   decoder->range = 510;
   decoder->bits_needed = 8;
@@ -172,8 +169,6 @@ void init_CABAC_decoder_2(CABAC_decoder* decoder)
 
   if (length>0) { decoder->value  = (*decoder->bitstream_curr++) << 8;  decoder->bits_needed-=8; }
   if (length>1) { decoder->value |= (*decoder->bitstream_curr++);       decoder->bits_needed-=8; }
-
-  fprintf(stderr,"VALUE: 0x%x\n",decoder->value);
 }
 
 
