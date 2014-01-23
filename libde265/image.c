5b8201f7565fdbab03f8ec08b15f45abdd5b6850
Joachim Bauch <mail@joachim-bauch.de>
2014-01-23 09:54:51 +0100

    Fixed undefined variable in assertion condition.

--- libde265/image.c
+++ libde265/image.c
@@ -350,8 +350,8 @@ int get_ctDepth(const de265_image* img, const seq_parameter_set* sps, int x,int 
 void set_QPY(de265_image* img, const seq_parameter_set* sps,
              const pic_parameter_set* pps, int x,int y, int QP_Y)
 {
-  assert(x>=0 && x<ctx->current_sps->pic_width_in_luma_samples);
-  assert(y>=0 && y<ctx->current_sps->pic_height_in_luma_samples);
+  assert(x>=0 && x<sps->pic_width_in_luma_samples);
+  assert(y>=0 && y<sps->pic_height_in_luma_samples);
 
   SET_CB_BLK_SAVE(x,y,pps->Log2MinCuQpDeltaSize, QP_Y, QP_Y);
 }
