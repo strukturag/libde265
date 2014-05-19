b316c807968001f018f9039f1ab9d6959f7e904e
Dirk Farin <dirk.farin@gmail.com>
2014-05-19 17:39:01 +0200

    add unhandled case in switch statement

--- libde265/image.cc
+++ libde265/image.cc
@@ -195,6 +195,10 @@ de265_error de265_image::alloc_image(int w,int h, enum de265_chroma c,
     spec.format = de265_image_format_YUV422P8;
     chroma_height = (chroma_height+1)/2;
     break;
+
+  default:
+    assert(false); // TODO: not implemented yet
+    break;
   }
 
   spec.width  = w;
