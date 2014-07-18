97285d993f07310ddca00b62c4074093a0cee43b
Dirk Farin <dirk.farin@gmail.com>
2014-07-18 14:44:09 +0200

    added enc265.cc source

---
+++ enc265/enc265.cc
@@ -0,0 +1,112 @@
+/*
+ * H.265 video codec.
+ * Copyright (c) 2013-2014 struktur AG, Dirk Farin <farin@struktur.de>
+ *
+ * This file is part of libde265.
+ *
+ * libde265 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libde265 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libde265.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "libde265/encode.h"
+#include "libde265/analyze.h"
+
+#include <getopt.h>
+
+
+int show_help=false;
+int verbosity=0;
+
+static struct option long_options[] = {
+  {"help",       no_argument,       &show_help, 1 },
+  {"verbose",    no_argument,       0, 'v' },
+  {0,            0,                 0,  0 }
+};
+
+
+
+int main(int argc, char** argv)
+{
+  encoder_context ectx;
+
+
+  // --- read encoder parameters ---
+
+  bool cmdline_errors = false;
+  config_parameters config_param;
+  register_encoder_params(&config_param);
+
+  FILE* reco_fh; // TODO
+
+  if (!config_param.parse_command_line_params(&argc,argv, &ectx.params, true)) {
+    cmdline_errors = true;
+  }
+
+
+
+  while (1) {
+    int option_index = 0;
+
+    int c = getopt_long(argc, argv, "v"
+                        , long_options, &option_index);
+    if (c == -1)
+      break;
+
+    switch (c) {
+    case 'v': verbosity++; break;
+    }
+  }
+
+
+  // --- show usage information ---
+
+  if (optind != argc || cmdline_errors || show_help) {
+    fprintf(stderr," enc265  v%s\n", de265_get_version());
+    fprintf(stderr,"--------------\n");
+    fprintf(stderr,"usage: enc265 [options]\n");
+    fprintf(stderr,"The video file must be a raw YUV file\n");
+    fprintf(stderr,"\n");
+    fprintf(stderr,"options:\n");
+    fprintf(stderr,"      --help         show help\n");
+    fprintf(stderr,"  -v, --verbose      increase verbosity level (up to 3 times)\n");
+
+    config_param.show_params();
+
+    exit(show_help ? 0 : 5);
+  }
+
+
+
+  // --- initialize encoder ---
+
+  init_scan_orders();
+  alloc_and_init_significant_coeff_ctxIdx_lookupTable();
+  init_acceleration_functions_fallback(&ectx.accel);
+
+  de265_set_verbosity(verbosity);
+
+  reco_fh = fopen(ectx.params.reconstruction_yuv,"wb");
+
+  encode_sequence(&ectx,
+                  ectx.params.input_yuv, ectx.params.input_width, ectx.params.input_height);
+
+  fclose(reco_fh);
+
+
+  FILE* fh = fopen(ectx.params.output_filename,"wb");
+  //fwrite(writer.data(), 1,writer.size(), fh); // TODO
+  fclose(fh);
+
+
+  return 0;
+}
