d240fc42ce4bcffe39a0650e174f9ec46d124705
Christian <christian.feldmann@gmx.de>
2015-05-08 11:59:12 +0200

    Added decoder_context_multilayer

---
+++ libde265/decctx-multilayer.cc
@@ -0,0 +1,13 @@
+#include "decctx-multilayer.h"
+
+decoder_context_multilayer::decoder_context_multilayer()
+{
+  // Init by creating one decoder context (there has to be at least one layer in the bitstream)
+  num_layer_decoders = 1;
+  layer_decoders[0] = new decoder_context;
+  //layer_decoders[0]->set_layer_id(0);
+  //layer_decoders[0]->set_decoder_ctx_array(layer_decoders);
+  //layer_decoders[0]->set_multilayer_decode_parameters( &ml_dec_params );
+  for (int i=1; i<MAX_LAYER_ID; i++)
+    layer_decoders[i] = NULL;
+}
---
+++ libde265/decctx-multilayer.h
@@ -0,0 +1,39 @@
+/*
+ * H.265 video codec.
+ * Copyright (c) 2013-2014 struktur AG, Dirk Farin <farin@struktur.de>
+ *
+ * This file is part of libde265.
+ *
+ * libde265 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * libde265 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libde265.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DE265_DECCTX_MULTILAYER_H
+#define DE265_DECCTX_MULTILAYER_H
+
+#define MAX_LAYER_ID 63  // Maximum number of layer supported by the standard (see vps_max_layers_minus1)
+
+#include "libde265/decctx.h"
+
+class decoder_context_multilayer {
+public:
+  decoder_context_multilayer();
+  ~decoder_context_multilayer();
+
+protected:
+  decoder_context* layer_decoders[MAX_LAYER_ID];
+  int num_layer_decoders;
+};
+
+
+#endif
