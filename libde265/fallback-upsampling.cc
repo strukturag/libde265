ac30fa7c764bb077e8a0253576ba1dd1a92251d1
Christian <christian.feldmann@gmx.de>
2015-05-27 21:53:49 +0200

    Added faster upsampling fallback methods. These are faster, but consume more temporary storage for temporal results.

--- libde265/fallback-upsampling.cc
+++ libde265/fallback-upsampling.cc
@@ -21,20 +21,30 @@
 #include "fallback-upsampling.h"
 #include "util.h"
 
+// Use a faster implementation of the upsampling filter. The downside is, that this upsampling 
+// implementation allocates a temporary buffer. This buffer has dimension SrcPicHeigh * DestPicWidth
+#define USE_FASTER_IMPLEMENTATION 1
+
 void resampling_process_of_luma_sample_values_fallback( uint8_t *src, ptrdiff_t srcstride, int src_size[2],
                                                         uint8_t *dst, ptrdiff_t dststride, int dst_size[2],
                                                         int position_params[8], int BitDepthRefLayerY, int BitDepthCurrY)
 {
+  // Reference layer size
   int PicHeightInSamplesRefLayerY = src_size[1];
   int PicWidthInSamplesRefLayerY = src_size[0];
 
+  // Current layer size
+  int PicHeightInSamplesCurLayerY = dst_size[1];
+  int PicWidthInSamplesCurLayerY = dst_size[0];
+
   int xRef16, yRef16, xRef, xPhase, yRef, yPhase;
   int shift1, shift2, offset;
-  int yPosRL, refW;
-  int tempArray[8];
+  int yPosRL;
   uint8_t *rlPicSampleL;
   uint8_t *rsLumaSample;
-  
+
+  int refW = PicWidthInSamplesRefLayerY;   // (H 37)
+
   // Table H.1 – 16-phase luma resampling filter
   int fL[16][8] = { { 0, 0,   0, 64,  0,   0, 0,  0},
                     { 0, 1,  -3, 63,  4,  -2, 1,  0},
@@ -52,9 +62,88 @@ void resampling_process_of_luma_sample_values_fallback( uint8_t *src, ptrdiff_t 
                     { 0, 1,  -4, 13, 60,  -8, 3, -1},
                     { 0, 1,  -3,  8, 62,  -5, 2, -1},
                     { 0, 1,  -2,  4, 63,  -3, 1,  0} };
+
+  // 4. The variables shift1, shift2 and offset are derived as follows:
+  shift1 = BitDepthRefLayerY - 8;  // (H 33)
+  shift2 = 20 - BitDepthCurrY;     // (H 34)
+  offset = 1 << (shift2 - 1);      // (H 35)
   
+#if USE_FASTER_IMPLEMENTATION
+  // Perform horizontal / vertical upsampling seperately.
+  // Allocate temporaray buffer
+  int16_t *tmp = new int16_t[PicHeightInSamplesRefLayerY * PicWidthInSamplesCurLayerY];
+  int16_t *tmpSample;
+  int tmpStride = PicWidthInSamplesCurLayerY;
+
+  // Horizontal upsampling
+  for (int y = 0; y < PicHeightInSamplesRefLayerY; y++) {
+
+    // Get pointer to the source for this y position
+    rlPicSampleL = src + y * srcstride;
+    // Get pointer to the temp array for this y position
+    tmpSample = tmp + y * tmpStride;
+
+    for (int xP = 0; xP < PicWidthInSamplesCurLayerY; xP++) {
+      // 1.
+      // H.8.1.4.1.3 Derivation process for reference layer sample location in units of 1/16-th sample
+      // The position_params array contains the precomputed values needed for this.
+      xRef16 = (((xP - position_params[0]) * position_params[4] + position_params[6] + (1 << 11)) >> 12) + position_params[2];  // (H 63)
+        
+      // 2. The variables xRef and xPhase are derived as follows:
+      xRef   = xRef16 >> 4;  // (H 29)
+      xPhase = xRef16 % 16;  // (H 30)
+
+      tmpSample[xP] = (fL[xPhase][0] * rlPicSampleL[ Clip3(0, refW - 1, xRef - 3)] +
+                       fL[xPhase][1] * rlPicSampleL[ Clip3(0, refW - 1, xRef - 2)] +
+                       fL[xPhase][2] * rlPicSampleL[ Clip3(0, refW - 1, xRef - 1)] +
+                       fL[xPhase][3] * rlPicSampleL[ Clip3(0, refW - 1, xRef    )] +
+                       fL[xPhase][4] * rlPicSampleL[ Clip3(0, refW - 1, xRef + 1)] +
+                       fL[xPhase][5] * rlPicSampleL[ Clip3(0, refW - 1, xRef + 2)] +
+                       fL[xPhase][6] * rlPicSampleL[ Clip3(0, refW - 1, xRef + 3)] +
+                       fL[xPhase][7] * rlPicSampleL[ Clip3(0, refW - 1, xRef + 4)] ) >> shift1; // (H 38)
+    }
+  }
+  
+  // Vertical upsampling
+  int refY = PicHeightInSamplesRefLayerY;
+
+  for (int yP = 0; yP < PicHeightInSamplesCurLayerY; yP++) {
+    // Get pointer to destination y line
+    rsLumaSample = dst + yP * dststride;
+    
+    // 1.
+    // H.8.1.4.1.3 Derivation process for reference layer sample location in units of 1/16-th sample
+    // The position_params array contains the precomputed values needed for this.
+    yRef16 = (((yP - position_params[1]) * position_params[5] + position_params[7] + (1 << 11)) >> 12) + position_params[3];  // (H 64)
+
+    // 3. The variables yRef and yPhase are derived as follows:
+    yPhase = yRef16 % 16;  // (H 32)
+    yRef   = yRef16 >> 4;  // (H 31)
+
+    for (int x = 0; x < PicWidthInSamplesCurLayerY; x++) {
+      // Get pointer to temp array y line
+      tmpSample = tmp + x;
+
+       // 6. The resampled luma sample value rsLumaSample is derived as follows:
+      rsLumaSample[x] = ( fL[yPhase][0] * tmpSample[ Clip3(0, refY - 1, yRef - 3) * tmpStride ] +
+                          fL[yPhase][1] * tmpSample[ Clip3(0, refY - 1, yRef - 2) * tmpStride ] +
+                          fL[yPhase][2] * tmpSample[ Clip3(0, refY - 1, yRef - 1) * tmpStride ] +
+                          fL[yPhase][3] * tmpSample[ Clip3(0, refY - 1, yRef    ) * tmpStride ] +
+                          fL[yPhase][4] * tmpSample[ Clip3(0, refY - 1, yRef + 1) * tmpStride ] +
+                          fL[yPhase][5] * tmpSample[ Clip3(0, refY - 1, yRef + 2) * tmpStride ] +
+                          fL[yPhase][6] * tmpSample[ Clip3(0, refY - 1, yRef + 3) * tmpStride ] +
+                          fL[yPhase][7] * tmpSample[ Clip3(0, refY - 1, yRef + 4) * tmpStride ] + offset ) >> shift2;  // (H 39)
+    }
+  }
+  
+  delete[] tmp;
+  tmp = NULL;
+
+#else
+  int tempArray[8];
+
   // H.8.1.4.1.1 Resampling process of luma sample values
-  for (int yP = 0; yP < dst_size[1]; yP++) {
+  for (int yP = 0; yP < PicHeightInSamplesCurLayerY; yP++) {
     rsLumaSample = dst + yP * dststride;
 
     // 1.
@@ -66,7 +155,7 @@ void resampling_process_of_luma_sample_values_fallback( uint8_t *src, ptrdiff_t 
     yPhase = yRef16 % 16;  // (H 32)
     yRef   = yRef16 >> 4;  // (H 31)
 
-    for (int xP = 0; xP < dst_size[0]; xP++) {
+    for (int xP = 0; xP < PicWidthInSamplesCurLayerY; xP++) {
       // 1.
       // H.8.1.4.1.3 Derivation process for reference layer sample location in units of 1/16-th sample
       // The position_params array contains the precomputed values needed for this.
@@ -75,16 +164,10 @@ void resampling_process_of_luma_sample_values_fallback( uint8_t *src, ptrdiff_t 
       // 2. The variables xRef and xPhase are derived as follows:
       xRef   = xRef16 >> 4;  // (H 29)
       xPhase = xRef16 % 16;  // (H 30)
-      
-      // 4. The variables shift1, shift2 and offset are derived as follows:
-      shift1 = BitDepthRefLayerY - 8;  // (H 33)
-      shift2 = 20 - BitDepthCurrY;     // (H 34)
-      offset = 1 << (shift2 - 1);      // (H 35)
 
       // 5. The sample value tempArray[ n ] with n = 0..7, is derived as follows:
       for (int n = 0; n<8; n++) {
         yPosRL = Clip3( 0, PicHeightInSamplesRefLayerY - 1, yRef + n - 3 );  // (H 36)
-        refW   = PicWidthInSamplesRefLayerY;                                 // (H 37)
 
         rlPicSampleL = src + yPosRL * srcstride;
         tempArray[n] = (fL[xPhase][0] * rlPicSampleL[ Clip3(0, refW - 1, xRef - 3)] +
@@ -108,6 +191,7 @@ void resampling_process_of_luma_sample_values_fallback( uint8_t *src, ptrdiff_t 
                             fL[yPhase][7] * tempArray[7] + offset ) >> shift2;  // (H 39)
     }
   }
+#endif
 }
 void resampling_process_of_chroma_sample_values_fallback( uint8_t *src, ptrdiff_t srcstride, int src_size[2],
                                                           uint8_t *dst, ptrdiff_t dststride, int dst_size[2],
@@ -115,13 +199,23 @@ void resampling_process_of_chroma_sample_values_fallback( uint8_t *src, ptrdiff_
 {
   int PicHeightInSamplesRefLayerC = src_size[1];
   int PicWidthInSamplesRefLayerC = src_size[0];
+
+  int PicHeightInSamplesCurLayerC = dst_size[1];
+  int PicWidthInSamplesCurLayerC = dst_size[0];
   
   int xRef16, yRef16, xRef, xPhase, yRef, yPhase;
   int shift1, shift2, offset;
-  int yPosRL, refWC;
+  int yPosRL;
   int tempArray[4];
   uint8_t *rlPicSampleC;
   uint8_t *rsChromaSample;
+
+  // 4. The variables shift1, shift2 and offset are derived as follows:
+  shift1 = BitDepthRefLayerC - 8;  // (H 45)
+  shift2 = 20 - BitDepthCurrC;     // (H 46)
+  offset = 1 << (shift2 - 1);      // (H 47)
+
+  int refWC = PicWidthInSamplesRefLayerC; // (H 49)
 
   // Table H.2 – 16-phase chroma resampling filter
   int fC[16][4] = { { 0, 64,  0,  0},
@@ -141,8 +235,75 @@ void resampling_process_of_chroma_sample_values_fallback( uint8_t *src, ptrdiff_
                     {-2, 10, 58, -2},
                     { 0,  4, 62, -2} };
 
+#if USE_FASTER_IMPLEMENTATION
+  // Perform horizontal / vertical upsampling seperately.
+  // Allocate temporaray buffer
+  int16_t *tmp = new int16_t[PicHeightInSamplesRefLayerC * PicWidthInSamplesCurLayerC];
+  int16_t *tmpSample;
+  int tmpStride = PicWidthInSamplesCurLayerC;
+
+  int refW = PicWidthInSamplesRefLayerC;   // (H 37)
+
+  // Horizontal upsampling
+  for (int y = 0; y < PicHeightInSamplesRefLayerC; y++) {
+
+    // Get pointer to the source for this y position
+    rlPicSampleC = src + y * srcstride;
+    // Get pointer to the temp array for this y position
+    tmpSample = tmp + y * tmpStride;
+
+    for (int xP = 0; xP < PicWidthInSamplesCurLayerC; xP++) {
+      // 1.
+      // H.8.1.4.1.3 Derivation process for reference layer sample location in units of 1/16-th sample
+      // The position_params array contains the precomputed values needed for this.
+      xRef16 = (((xP - position_params[0]) * position_params[4] + position_params[6] + (1 << 11)) >> 12) + position_params[2];  // (H 63)
+
+      // 2. The variables xRef and xPhase are derived as follows:
+      xRef   = xRef16 >> 4;  // (H 29)
+      xPhase = xRef16 % 16;  // (H 30)
+
+      tmpSample[xP] = (fC[xPhase][0] * rlPicSampleC[ Clip3(0, refWC - 1, xRef - 1)] +
+                       fC[xPhase][1] * rlPicSampleC[ Clip3(0, refWC - 1, xRef    )] +
+                       fC[xPhase][2] * rlPicSampleC[ Clip3(0, refWC - 1, xRef + 1)] +
+                       fC[xPhase][3] * rlPicSampleC[ Clip3(0, refWC - 1, xRef + 2)] ) >> shift1; // (H 50)
+    }
+  }
+
+  // Vertical upsampling
+  int refY = PicHeightInSamplesRefLayerC;
+
+  for (int yP = 0; yP < PicHeightInSamplesCurLayerC; yP++) {
+    // Get pointer to destination y line
+    rsChromaSample = dst + yP * dststride;
+    
+    // 1.
+    // H.8.1.4.1.3 Derivation process for reference layer sample location in units of 1/16-th sample
+    // The position_params array contains the precomputed values needed for this.
+    yRef16 = (((yP - position_params[1]) * position_params[5] + position_params[7] + (1 << 11)) >> 12) + position_params[3];  // (H 64)
+
+    // 3. The variables yRef and yPhase are derived as follows:
+    //yPhase = yRef16 % 16;  // (H 32)
+    yPhase = yRef16 & 15;  // This is what the reference software does. TODO: Double check with the latest standard.
+    yRef   = yRef16 >> 4;  // (H 31)
+
+    for (int x = 0; x < PicWidthInSamplesCurLayerC; x++) {
+      // Get pointer to temp array y line
+      tmpSample = tmp + x;
+
+      // 6. The resampled chroma sample value rsChromaSample is derived as follows:
+      rsChromaSample[x] = ( fC[yPhase][0] * tmpSample[ Clip3(0, refY - 1, yRef - 1) * tmpStride ] +
+                            fC[yPhase][1] * tmpSample[ Clip3(0, refY - 1, yRef    ) * tmpStride ] +
+                            fC[yPhase][2] * tmpSample[ Clip3(0, refY - 1, yRef + 1) * tmpStride ] +
+                            fC[yPhase][3] * tmpSample[ Clip3(0, refY - 1, yRef + 2) * tmpStride ] + offset ) >> shift2;  // (H 51)
+    }
+  }
+
+  delete[] tmp;
+  tmp = NULL;
+
+#else
   // H.8.1.4.1.2 Resampling process of chroma sample values
-  for (int yP = 0; yP < dst_size[1]; yP++) {
+  for (int yP = 0; yP < PicHeightInSamplesCurLayerC; yP++) {
     rsChromaSample = dst + yP * dststride;
 
     // 1.
@@ -155,7 +316,7 @@ void resampling_process_of_chroma_sample_values_fallback( uint8_t *src, ptrdiff_
     //yPhase = yRef16 % 16;  // (H 44)
     yPhase = yRef16 & 15;  // This is what the reference software does. TODO: Double check with the latest standard.
     
-    for (int xP = 0; xP < dst_size[0]; xP++) {
+    for (int xP = 0; xP < PicWidthInSamplesCurLayerC; xP++) {
       // 1.
       // H.8.1.4.1.3 Derivation process for reference layer sample location in units of 1/16-th sample
       // The position_params array contains the precomputed values needed for this.
@@ -164,16 +325,10 @@ void resampling_process_of_chroma_sample_values_fallback( uint8_t *src, ptrdiff_
       // 2. The variables xRef and xPhase are derived as follows:
       xRef   = xRef16 >> 4;  // (H 41)
       xPhase = xRef16 % 16;  // (H 42)
-      
-      // 4. The variables shift1, shift2 and offset are derived as follows:
-      shift1 = BitDepthRefLayerC - 8;  // (H 45)
-      shift2 = 20 - BitDepthCurrC;     // (H 46)
-      offset = 1 << (shift2 - 1);      // (H 47)
 
       // 5. The sample value tempArray[ n ] with n = 0..3, is derived as follows:
       for (int n = 0; n<4; n++) {
         yPosRL = Clip3( 0, PicHeightInSamplesRefLayerC - 1, yRef + n - 1 );  // (H 48)
-        refWC  = PicWidthInSamplesRefLayerC;                                 // (H 49)
 
         rlPicSampleC = src + yPosRL * srcstride;
         tempArray[n] = (fC[xPhase][0] * rlPicSampleC[ Clip3(0, refWC - 1, xRef - 1)] +
@@ -191,6 +346,7 @@ void resampling_process_of_chroma_sample_values_fallback( uint8_t *src, ptrdiff_
       rsChromaSample[xP] = Clip3(0, ( 1 << BitDepthCurrC ) - 1, rsChromaSample[xP]);  // (H 52)
     }
   }
+#endif
 }
 
 
