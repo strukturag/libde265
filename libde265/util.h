de0a96c703a02de836e6d70987f09a5cfe92bb18
Joachim Bauch <mail@joachim-bauch.de>
2015-06-26 11:23:06 +0200

    Support compilation on GCC < 4.6 (i.e. NaCl).

--- libde265/util.h
+++ libde265/util.h
@@ -29,6 +29,9 @@
 
 #include "libde265/de265.h"
 
+#ifdef __GNUC__
+#define GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
+#endif
 
 #ifdef _MSC_VER
 #define LIBDE265_DECLARE_ALIGNED( var, n ) __declspec(align(n)) var
@@ -46,16 +49,22 @@
 #define ALIGNED_4( var )  LIBDE265_DECLARE_ALIGNED( var, 4 )
 
 // C++11 specific features
-#ifdef _MSC_VER
+#if defined(_MSC_VER) || (__GNUC__ && GCC_VERSION < 40600)
 #define FOR_LOOP(type, var, list)   for each (type var in list)
 #undef FOR_LOOP_AUTO_SUPPORT
 #else
 #define FOR_LOOP(type, var, list)   for (type var : list)
 #define FOR_LOOP_AUTO_SUPPORT 1
 #endif
+
 #ifdef USE_STD_TR1_NAMESPACE
 #include <tr1/memory>
 namespace std { using namespace std::tr1; }
+#endif
+
+#if __GNUC__ && GCC_VERSION < 40600
+// nullptr was introduced in gcc 4.6, a simple alias should be fine for our use case
+#define nullptr NULL
 #endif
 
 #ifdef _MSC_VER
