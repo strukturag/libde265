8642a58c14c638a1609dde3ab583b94d06925770
Joachim Bauch <mail@joachim-bauch.de>
2015-07-02 17:50:00 +0200

    Also convert "stride" from "de265_set_image_plane" to internal value.

--- libde265/de265.cc
+++ libde265/de265.cc
@@ -638,6 +638,8 @@ LIBDE265_API void *de265_get_image_plane_user_data(const struct de265_image* img
 
 LIBDE265_API void de265_set_image_plane(de265_image* img, int cIdx, void* mem, int stride, void *userdata)
 {
+  // The internal "stride" is the number of pixels per line.
+  stride = stride / ((de265_get_bits_per_pixel(img, cIdx)+7) / 8);
   img->set_image_plane(cIdx, (uint8_t*)mem, stride, userdata);
 }
 
