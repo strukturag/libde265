7b6be7489cc62dd1706d5578c04a4edcefe48cbe
christian <christian.feldmann@gmx.de>
2015-11-04 12:02:27 +0100

    Bugfix: Ignore RASL pictures if the NoRASLOutput flag is set.

--- libde265/decctx.cc
+++ libde265/decctx.cc
@@ -2039,6 +2039,8 @@ bool decoder_context::process_slice_segment_header(slice_segment_header* hdr,
         NoRaslOutputFlag)
       {
         img->PicOutputFlag = false;
+        // This is a RASL but we are not going to output (decode) RASLs.
+        return false;
       }
     else
       {
@@ -2064,6 +2066,11 @@ bool decoder_context::process_slice_segment_header(slice_segment_header* hdr,
     first_decoded_picture = false;
   }
   else {
+    if (isRASL(nal_unit_type) && NoRaslOutputFlag) {
+      // Not the first slice, a RASL, and we are not outputting (decoding) RASLS.
+      return false;
+    }
+
     // claims to be not the first slice, but there is no active image available
 
     if (img == NULL) {
