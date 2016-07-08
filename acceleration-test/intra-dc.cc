7482c84697890e7cc419f278073a36d66f791673
Dirk Farin <farin@struktur.de>
2016-07-08 19:42:11 +0200

    intra dc 32x32 in SSE

--- acceleration-test/intra-dc.cc
+++ acceleration-test/intra-dc.cc
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
+#include <time.h>
 
 #define D 0
 
@@ -135,7 +136,7 @@ void __attribute__ ((noinline)) intra_dc_see_noavg_8bit_8x8(uint8_t* dst,int dst
 void __attribute__ ((noinline)) intra_dc_see_noavg_8bit_16x16(uint8_t* dst,int dstStride, uint8_t* border)
 {
   __m128i zero   = _mm_setzero_si128();
-  __m128i ones16 = _mm_set1_epi16(1);
+  __m128i ones16 = _mm_set1_epi16(2);
   __m128i leftborder = _mm_load_si128((const __m128i*)(border-16));
   __m128i topborder  = _mm_loadu_si128((const __m128i*)(border+1));
   //__m128i topborder  = _mm_load_si128((const __m128i*)(border));
@@ -187,6 +188,77 @@ void __attribute__ ((noinline)) intra_dc_see_noavg_8bit_16x16(uint8_t* dst,int d
 }
 
 
+// 1.97x faster
+void __attribute__ ((noinline)) intra_dc_see_noavg_8bit_32x32(uint8_t* dst,int dstStride, uint8_t* border)
+{
+  __m128i zero   = _mm_setzero_si128();
+  __m128i ones16 = _mm_set1_epi16(4);
+  __m128i leftborder_a = _mm_load_si128((const __m128i*)(border-16));
+  __m128i leftborder_b = _mm_load_si128((const __m128i*)(border-32));
+  __m128i topborder_a  = _mm_loadu_si128((const __m128i*)(border+1));
+  __m128i topborder_b  = _mm_loadu_si128((const __m128i*)(border+17));
+  //__m128i topborder  = _mm_load_si128((const __m128i*)(border));
+  //topborder = _mm_srli_si128(topborder, 1);
+
+  Deb(topborder_a);
+  Deb(topborder_b);
+  Deb(leftborder_a);
+  Deb(leftborder_b);
+  Deb(ones16);
+
+  __m128i topborder16_lo  = _mm_unpacklo_epi8(topborder_a ,zero);
+  __m128i leftborder16_lo = _mm_unpacklo_epi8(leftborder_a,zero);
+
+  __m128i topborder16_hi  = _mm_unpackhi_epi8(topborder_a ,zero);
+  __m128i leftborder16_hi = _mm_unpackhi_epi8(leftborder_a,zero);
+
+  __m128i topborder16b_lo  = _mm_unpacklo_epi8(topborder_b ,zero);
+  __m128i leftborder16b_lo = _mm_unpacklo_epi8(leftborder_b,zero);
+
+  __m128i topborder16b_hi  = _mm_unpackhi_epi8(topborder_b ,zero);
+  __m128i leftborder16b_hi = _mm_unpackhi_epi8(leftborder_b,zero);
+
+  Deb(topborder16_lo);
+  Deb(leftborder16_lo);
+  Deb(topborder16_hi);
+  Deb(leftborder16_hi);
+
+  __m128i bordersum_lo = _mm_add_epi16(topborder16_lo, leftborder16_lo);
+  __m128i bordersum_hi = _mm_add_epi16(topborder16_hi, leftborder16_hi);
+  __m128i bordersumb_lo = _mm_add_epi16(topborder16b_lo, leftborder16b_lo);
+  __m128i bordersumb_hi = _mm_add_epi16(topborder16b_hi, leftborder16b_hi);
+  bordersum_lo = _mm_add_epi16(bordersum_lo, bordersumb_lo);
+  bordersum_hi = _mm_add_epi16(bordersum_hi, bordersumb_hi);
+  __m128i bordersum_a = _mm_add_epi16(bordersum_lo, bordersum_hi);
+  __m128i bordersum_b = _mm_add_epi16(bordersum_a, ones16);
+
+  Deb(bordersum_a);
+  Deb(bordersum_b);
+
+  __m128i dcsum4 = _mm_hadd_epi16(bordersum_b, zero);
+  __m128i dcsum2 = _mm_hadd_epi16(dcsum4, zero);
+  __m128i dcsum1 = _mm_hadd_epi16(dcsum2, zero);
+
+  Deb(dcsum4);
+  Deb(dcsum2);
+  Deb(dcsum1);
+
+  const int log_nT = 5;
+  __m128i dcsum = _mm_srai_epi16(dcsum1, log_nT+1);
+
+  Deb(dcsum);
+
+  __m128i flatdc = _mm_shuffle_epi8(dcsum, zero);
+
+  Deb(flatdc);
+
+  for (int y=0;y<32;y++) {
+    _mm_store_si128((__m128i*)(dst+y*dstStride   ), flatdc);
+    _mm_store_si128((__m128i*)(dst+y*dstStride+16), flatdc);
+  }
+}
+
+
 template <class pixel_t>
 void __attribute__ ((noinline)) intra_dc_fallback_8bit(pixel_t* dst,int dstStride, pixel_t* border, int Log2_nT, bool avg)
 {
@@ -231,7 +303,7 @@ void __attribute__ ((noinline)) intra_dc_fallback_8bit(pixel_t* dst,int dstStrid
 
 void intra_dc()
 {
-  const int w = 4;
+  const int w = 32;
 
   int imgw=640,imgh=320;
 
@@ -245,21 +317,35 @@ void intra_dc()
   border[2*w] = 0xC0;
 
 
+  srand(time(0));
+  for (int i=0;i<4*w+1;i++) {
+    border[i] = rand()&0xFF;
+  }
+
   for (int i=0;i<2*2*w+1;i++) {
     printf("%02x ", border[i]);
   }
   printf("\n");
 
-  for (int i=0;i<1000000;i++) {
-    for (int j=0;j<10000;j++) {
-      //intra_dc_fallback_8bit(dstc,imgw, border+2*w, 2, false);
+  for (int i=0;i<100000000;i++) {
+    if (w==4) {
+      intra_dc_fallback_8bit(dstc,imgw, border+2*w, 2, false);
       intra_dc_see_noavg_8bit_4x4(dstsse,imgw, border+2*w);
+    }
 
-      //intra_dc_fallback_8bit(dstc,imgw, border+2*w, 3, false);
-      //intra_dc_see_noavg_8bit_8x8(dstsse,imgw, border+2*w);
+    if (w==8) {
+      intra_dc_fallback_8bit(dstc,imgw, border+2*w, 3, false);
+      intra_dc_see_noavg_8bit_8x8(dstsse,imgw, border+2*w);
+    }
 
-      //intra_dc_fallback_8bit(dstc,imgw, border+2*w, 4, false);
-      //intra_dc_see_noavg_8bit_16x16(dstsse,imgw, border+2*w);
+    if (w==16) {
+      intra_dc_fallback_8bit(dstc,imgw, border+2*w, 4, false);
+      intra_dc_see_noavg_8bit_16x16(dstsse,imgw, border+2*w);
+    }
+
+    if (w==32) {
+      //intra_dc_fallback_8bit(dstc,imgw, border+2*w, 5, false);
+      intra_dc_see_noavg_8bit_32x32(dstsse,imgw, border+2*w);
     }
   }
 
