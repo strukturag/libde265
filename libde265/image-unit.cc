0cb9d1d86587621350ae0fcbad518fd0a36fb55f
Dirk Farin <farin@struktur.de>
2017-01-18 12:44:28 +0100

    oops, two printf from debug-branch slipped through into this branch

--- libde265/image-unit.cc
+++ libde265/image-unit.cc
@@ -65,8 +65,6 @@ void slice_unit::mark_covered_CTBs_as_processed(int progress) const
 {
   // mark all CTBs assigned to this slice as processed
 
-  printf("mark covered range: %d - %d\n", first_CTB_TS, last_CTB_TS);
-
   for (int ctb=first_CTB_TS; ctb <= last_CTB_TS; ctb++)
     {
       if (ctb >= imgunit->img->number_of_ctbs())
@@ -74,7 +72,6 @@ void slice_unit::mark_covered_CTBs_as_processed(int progress) const
 
       int ctb_rs = shdr->get_pps()->CtbAddrTStoRS[ctb];
 
-      printf("mark progress RS:%d (TS:%d) = %d\n",ctb_rs,ctb, progress);
       imgunit->img->ctb_progress[ctb_rs].set_progress(progress);
     }
 }
