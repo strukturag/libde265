57d595098b2d2eb4fecc018a4e97e82a89378c20
Dirk Farin <farin@struktur.de>
2017-09-27 15:49:16 +0200

    SOP low delay: create headers and set RefPicSet

--- libde265/encoder/sop.cc
+++ libde265/encoder/sop.cc
@@ -122,29 +122,80 @@ sop_creator_trivial_low_delay::sop_creator_trivial_low_delay()
 
 void sop_creator_trivial_low_delay::insert_new_input_image(image_ptr img)
 {
-  /* TODO
+  // --- allocate headers, if not done already
 
-  ref_pic_set rps;
-  rps.DeltaPocS0[0] = -1;
-  rps.UsedByCurrPicS0[0] = true;
-  rps.NumNegativePics = 1;
-  rps.NumPositivePics = 0;
-  rps.compute_derived_values();
-  sps->ref_pic_sets.push_back(rps);
-  sps->log2_max_pic_order_cnt_lsb = get_num_poc_lsb_bits();
-   */
+  if (!pps) {
+    vps = std::make_shared<video_parameter_set>();
+    sps = std::make_shared<seq_parameter_set>();
+    pps = std::make_shared<pic_parameter_set>();
+
+    vps->set_defaults(Profile_Main, 6,2);
+    vps->video_parameter_set_id = 0;
+
+    sps->set_defaults();
+    sps->seq_parameter_set_id = 0;
+    sps->video_parameter_set_id = 0;
+
+    pps->set_defaults();
+    pps->pic_parameter_set_id = 0;
+    pps->seq_parameter_set_id = 0;
+    pps->sps = sps;
+
+
+    // --- set image size
+
+    sps->pic_width_in_luma_samples  = img->get_width();
+    sps->pic_height_in_luma_samples = img->get_height();
+    sps->compute_derived_values();
+
+
+    // --- set SPS RefPicSets
+
+    ref_pic_set rps;
+    rps.addRefL0(-1, ref_pic_set::image_mode::used);
+
+    rps.DeltaPocS0[0] = -1;
+    rps.UsedByCurrPicS0[0] = true;
+    rps.NumNegativePics = 1;
+    rps.NumPositivePics = 0;
+    rps.compute_derived_values();
+
+    sps->ref_pic_sets.push_back(rps);
+
+
+    // --- set some more default values ---
+
+    sps->log2_max_pic_order_cnt_lsb = get_num_poc_lsb_bits();
+
+    sps->pcm_enabled_flag = false;
+
+
+    // turn off deblocking filter
+    pps->deblocking_filter_control_present_flag = true;
+    pps->deblocking_filter_override_enabled_flag = false;
+    pps->pic_disable_deblocking_filter_flag = true;
+    pps->pps_loop_filter_across_slices_enabled_flag = false;
+
+    pps->set_derived_values(sps.get());
+
+
+    mEncCtx->fill_headers(vps,sps,pps, img);
+  }
+
+
+  // --- initialize picture_encoding_data
 
   img->PicOrderCntVal = get_pic_order_count();
+
 
   // fake input image state, so that we can use it for MC in some fast algorithms
   img->PicState = UsedForShortTermReference;
 
-  int frame = get_frame_number();
+  int frame_num = get_frame_number();
 
-  std::vector<int> l0, l1, empty;
-  if (!isIntra(frame)) {
-    l0.push_back(frame-1);
-  }
+
+
+  // picture data
 
   assert(mEncPicBuf);
   auto imgdata = std::make_shared<picture_encoding_data>(img,
@@ -159,25 +210,26 @@ void sop_creator_trivial_low_delay::insert_new_input_image(image_ptr img)
 
   imgdata->reconstruction->add_slice_segment_header(shdr);
 
-  if (isIntra(frame)) {
+  if (isIntra(frame_num)) {
     reset_poc();
     imgdata->set_NAL_type(NAL_UNIT_IDR_N_LP);
     shdr->slice_type = SLICE_TYPE_I;
   } else {
+
+    // fill list l0 with preceding image
+
+    std::vector<int> l0, l1, empty;
+    l0.push_back(frame_num - 1);
+
+
+    // set RefPicList in slice header
+
     shdr->num_ref_idx_l0_active = l0.size();
-    //shdr.num_ref_idx_l1_active = l1.size();
 
     assert(l0.size() < MAX_NUM_REF_PICS);
     for (int i=0;i<l0.size();i++) {
       shdr->RefPicList[0][i] = l0[i];
     }
-
-    /*
-      assert(l1.size() < MAX_NUM_REF_PICS);
-      for (int i=0;i<l1.size();i++) {
-      shdr.RefPicList[1][i] = l1[i];
-      }
-    */
 
 
     imgdata->set_references(0, l0,l1, empty,empty);
