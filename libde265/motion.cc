11b95ec6384cc29469ceed6b4741927b2195aa34
Dirk Farin <farin@struktur.de>
2017-09-28 18:07:26 +0200

    reduce logging noise

--- libde265/motion.cc
+++ libde265/motion.cc
@@ -878,10 +878,10 @@ void logmvcand(const PBMotion& p)
 {
   for (int v=0;v<2;v++) {
     if (p.predFlag[v]) {
-      logdebug(LogMotion,"  %d: %s  %d;%d ref=%d\n", v, p.predFlag[v] ? "yes":"no ",
+      logtrace(LogMotion,"  %d: %s  %d;%d ref=%d\n", v, p.predFlag[v] ? "yes":"no ",
                p.mv[v].x,p.mv[v].y, p.refIdx[v]);
     } else {
-      logdebug(LogMotion,"  %d: %s  --;-- ref=--\n", v, p.predFlag[v] ? "yes":"no ");
+      logtrace(LogMotion,"  %d: %s  --;-- ref=--\n", v, p.predFlag[v] ? "yes":"no ");
     }
   }
 }
