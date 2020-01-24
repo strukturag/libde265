b780d82e296aeace6bb8491efe86c73123cd52ee
mave2802 <59919483+mave2802@users.noreply.github.com>
2020-01-24 13:40:53 +0100

    Add support for hrd parameters in Video usability information

--- libde265/vui.cc
+++ libde265/vui.cc
@@ -128,8 +128,7 @@ video_usability_information::video_usability_information()
   // --- hrd parameters ---
 
   vui_hrd_parameters_present_flag = false;
-  //hrd_parameters vui_hrd_parameters;
-
+ 
 
   // --- bitstream restriction ---
 
@@ -145,6 +144,87 @@ video_usability_information::video_usability_information()
 }
 
 
+de265_error video_usability_information::hrd_parameters(error_queue* errqueue, bitreader* br, const seq_parameter_set* sps)
+{
+  int vlc;
+
+  nal_hrd_parameters_present_flag = get_bits(br, 1);
+  vcl_hrd_parameters_present_flag = get_bits(br, 1);
+
+  if (nal_hrd_parameters_present_flag || vcl_hrd_parameters_present_flag)
+  {
+    sub_pic_hrd_params_present_flag = get_bits(br, 1);
+    if (sub_pic_hrd_params_present_flag)
+    {
+      tick_divisor_minus2 = get_bits(br, 8);
+      du_cpb_removal_delay_increment_length_minus1 = get_bits(br, 5);
+      sub_pic_cpb_params_in_pic_timing_sei_flag = get_bits(br, 1);
+      dpb_output_delay_du_length_minus1 = get_bits(br, 5);
+    }
+    bit_rate_scale = get_bits(br, 4);
+    cpb_size_scale = get_bits(br, 4);
+
+
+    if (sub_pic_hrd_params_present_flag)
+    {
+      cpb_size_du_scale = get_bits(br, 4);
+    }
+    initial_cpb_removal_delay_length_minus1 = get_bits(br, 5);
+    au_cpb_removal_delay_length_minus1 = get_bits(br, 5);
+    dpb_output_delay_length_minus1 = get_bits(br, 5);
+  }
+  int  i, j, nalOrVcl;
+
+  for (i = 0; i < sps->sps_max_sub_layers; i++)
+  {
+    fixed_pic_rate_general_flag[i] = get_bits(br, 1);
+    if (!fixed_pic_rate_general_flag[i])
+    {
+      fixed_pic_rate_within_cvs_flag[i] = get_bits(br, 1);
+    }
+    else
+    {
+      fixed_pic_rate_within_cvs_flag[i] = true;
+    }
+
+    low_delay_hrd_flag[i] = 0;// Infered to be 0 when not present
+    cpb_cnt_minus1[i] = 0;    // Infered to be 0 when not present
+
+    if (fixed_pic_rate_within_cvs_flag[i])
+    {
+      READ_VLC_OFFSET(elemental_duration_in_tc_minus1[i], uvlc, 0);
+    }
+    else
+    {
+      low_delay_hrd_flag[i] = get_bits(br, 1);
+    }
+    if (!low_delay_hrd_flag[i])
+    {
+      READ_VLC_OFFSET(cpb_cnt_minus1[i], uvlc, 0);
+    }
+
+    for (nalOrVcl = 0; nalOrVcl < 2; nalOrVcl++)
+    {
+      if (((nalOrVcl == 0) && nal_hrd_parameters_present_flag) ||
+        ((nalOrVcl == 1) && vcl_hrd_parameters_present_flag))
+      {
+        for (j = 0; j <= cpb_cnt_minus1[i]; j++)
+        {
+          READ_VLC_OFFSET(bit_rate_value_minus1[i][j][nalOrVcl], uvlc, 0);
+          READ_VLC_OFFSET(cpb_size_value_minus1[i][j][nalOrVcl], uvlc, 0);
+
+          if (sub_pic_hrd_params_present_flag)
+          {
+            READ_VLC_OFFSET(cpb_size_du_value_minus1[i][j][nalOrVcl], uvlc, 0);
+            READ_VLC_OFFSET(bit_rate_du_value_minus1[i][j][nalOrVcl], uvlc, 0);
+          }
+          cbr_flag[i][j][nalOrVcl] = get_bits(br, 1);
+        }
+      }
+    }
+  }
+}
+
 de265_error video_usability_information::read(error_queue* errqueue, bitreader* br,
                                               const seq_parameter_set* sps)
 {
@@ -153,33 +233,33 @@ de265_error video_usability_information::read(error_queue* errqueue, bitreader* 
 
   // --- sample aspect ratio (SAR) ---
 
-  aspect_ratio_info_present_flag = get_bits(br,1);
+  aspect_ratio_info_present_flag = get_bits(br, 1);
   if (aspect_ratio_info_present_flag) {
-    int aspect_ratio_idc = get_bits(br,8);
+    int aspect_ratio_idc = get_bits(br, 8);
     if (aspect_ratio_idc <= NUM_SAR_PRESETS) {
-      sar_width  = sar_presets[aspect_ratio_idc][0];
+      sar_width = sar_presets[aspect_ratio_idc][0];
       sar_height = sar_presets[aspect_ratio_idc][1];
     }
     else if (aspect_ratio_idc == EXTENDED_SAR) {
-      sar_width  = get_bits(br,16);
-      sar_height = get_bits(br,16);
+      sar_width = get_bits(br, 16);
+      sar_height = get_bits(br, 16);
     }
     else {
-      sar_width  = 0;
+      sar_width = 0;
       sar_height = 0;
     }
   }
   else {
-    sar_width  = 0;
+    sar_width = 0;
     sar_height = 0;
   }
 
 
   // --- overscan ---
 
-  overscan_info_present_flag = get_bits(br,1);
+  overscan_info_present_flag = get_bits(br, 1);
   if (overscan_info_present_flag) {
-    overscan_appropriate_flag = get_bits(br,1);
+    overscan_appropriate_flag = get_bits(br, 1);
   }
 
 
@@ -193,35 +273,35 @@ de265_error video_usability_information::read(error_queue* errqueue, bitreader* 
     matrix_coeffs = 2;
   }
 
-  video_signal_type_present_flag = get_bits(br,1);
+  video_signal_type_present_flag = get_bits(br, 1);
   if (video_signal_type_present_flag) {
-    int video_format_idc = get_bits(br,3);
+    int video_format_idc = get_bits(br, 3);
     if (video_format_idc > 5) {
       video_format_idc = VideoFormat_Unspecified;
     }
     video_format = (VideoFormat)video_format_idc;
 
-    video_full_range_flag = get_bits(br,1);
+    video_full_range_flag = get_bits(br, 1);
 
-    colour_description_present_flag = get_bits(br,1);
+    colour_description_present_flag = get_bits(br, 1);
     if (colour_description_present_flag) {
-      colour_primaries = get_bits(br,8);
+      colour_primaries = get_bits(br, 8);
       if (colour_primaries == 0 ||
-          colour_primaries == 3 ||
-          colour_primaries >= 11) {
+        colour_primaries == 3 ||
+        colour_primaries >= 11) {
         colour_primaries = 2;
       }
 
-      transfer_characteristics = get_bits(br,8);
+      transfer_characteristics = get_bits(br, 8);
       if (transfer_characteristics == 0 ||
-          transfer_characteristics == 3 ||
-          transfer_characteristics >= 18) {
+        transfer_characteristics == 3 ||
+        transfer_characteristics >= 18) {
         transfer_characteristics = 2;
       }
 
-      matrix_coeffs = get_bits(br,8);
+      matrix_coeffs = get_bits(br, 8);
       if (matrix_coeffs == 0 ||
-          matrix_coeffs >= 11) {
+        matrix_coeffs >= 11) {
         matrix_coeffs = 2;
       }
     }
@@ -230,58 +310,58 @@ de265_error video_usability_information::read(error_queue* errqueue, bitreader* 
 
   // --- chroma / interlaced ---
 
-  chroma_loc_info_present_flag = get_bits(br,1);
+  chroma_loc_info_present_flag = get_bits(br, 1);
   if (chroma_loc_info_present_flag) {
-    READ_VLC(chroma_sample_loc_type_top_field,    uvlc);
+    READ_VLC(chroma_sample_loc_type_top_field, uvlc);
     READ_VLC(chroma_sample_loc_type_bottom_field, uvlc);
   }
   else {
-    chroma_sample_loc_type_top_field    = 0;
+    chroma_sample_loc_type_top_field = 0;
     chroma_sample_loc_type_bottom_field = 0;
   }
 
-  neutral_chroma_indication_flag = get_bits(br,1);
-  field_seq_flag                 = get_bits(br,1);
-  frame_field_info_present_flag  = get_bits(br,1);
+  neutral_chroma_indication_flag = get_bits(br, 1);
+  field_seq_flag = get_bits(br, 1);
+  frame_field_info_present_flag = get_bits(br, 1);
 
 
   // --- default display window ---
 
-  default_display_window_flag = get_bits(br,1);
+  default_display_window_flag = get_bits(br, 1);
   if (default_display_window_flag) {
-    READ_VLC(def_disp_win_left_offset  ,uvlc);
-    READ_VLC(def_disp_win_right_offset ,uvlc);
-    READ_VLC(def_disp_win_top_offset   ,uvlc);
-    READ_VLC(def_disp_win_bottom_offset,uvlc);
+    READ_VLC(def_disp_win_left_offset, uvlc);
+    READ_VLC(def_disp_win_right_offset, uvlc);
+    READ_VLC(def_disp_win_top_offset, uvlc);
+    READ_VLC(def_disp_win_bottom_offset, uvlc);
   }
   else {
-    def_disp_win_left_offset  =0;
-    def_disp_win_right_offset =0;
-    def_disp_win_top_offset   =0;
-    def_disp_win_bottom_offset=0;
+    def_disp_win_left_offset = 0;
+    def_disp_win_right_offset = 0;
+    def_disp_win_top_offset = 0;
+    def_disp_win_bottom_offset = 0;
   }
 
 
   // --- timing ---
 
-  vui_timing_info_present_flag = get_bits(br,1);
+  vui_timing_info_present_flag = get_bits(br, 1);
   if (vui_timing_info_present_flag) {
-    vui_num_units_in_tick = get_bits(br,32);
-    vui_time_scale        = get_bits(br,32);
+    vui_num_units_in_tick = get_bits(br, 32);
+    vui_time_scale = get_bits(br, 32);
+
+    vui_poc_proportional_to_timing_flag = get_bits(br, 1);
+    if (vui_poc_proportional_to_timing_flag) {
+      READ_VLC_OFFSET(vui_num_ticks_poc_diff_one, uvlc, 1);
+    }
+
+    // --- hrd parameters ---
+
+    vui_hrd_parameters_present_flag = get_bits(br, 1);
+    if (vui_hrd_parameters_present_flag) {
+      de265_error err;
+      err = hrd_parameters(errqueue, br, sps);
+    }
   }
-
-  vui_poc_proportional_to_timing_flag = get_bits(br,1);
-  READ_VLC_OFFSET(vui_num_ticks_poc_diff_one, uvlc, 1);
-
-
-  // --- hrd parameters ---
-
-  vui_hrd_parameters_present_flag = get_bits(br,1);
-  if (vui_hrd_parameters_present_flag) {
-    return DE265_ERROR_NOT_IMPLEMENTED_YET;
-    //hrd_parameters vui_hrd_parameters;
-  }
-
 
   // --- bitstream restriction ---
 
