f538254e4658ef5ea4e233c2185dcbfd165e8911
Dirk Farin <dirk.farin@gmail.com>
2022-04-05 18:41:28 +0200

    fix streams where SPS image size changes without refreshing PPS (#299)

--- libde265/decctx.cc
+++ libde265/decctx.cc
@@ -562,6 +562,15 @@ de265_error decoder_context::read_sps_NAL(bitreader& reader)
 
   sps[ new_sps->seq_parameter_set_id ] = new_sps;
 
+  // Remove the all PPS that referenced the old SPS because parameters may have changed and we do not want to
+  // get the SPS and PPS parameters (e.g. image size) out of sync.
+  
+  for (auto& p : pps) {
+    if (p && p->seq_parameter_set_id == new_sps->seq_parameter_set_id) {
+      p = nullptr;
+    }
+  }
+
   return DE265_OK;
 }
 
