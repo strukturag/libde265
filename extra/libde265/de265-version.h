7065a5304bff5b024fbbb55f2d5cee1c4ad988fd
Dirk Farin <dirk.farin@gmail.com>
2023-11-21 10:16:36 +0100

    increase version to v1.0.14

--- extra/libde265/de265-version.h
+++ extra/libde265/de265-version.h
@@ -22,8 +22,8 @@
 #define LIBDE265_VERSION_H
 
 /* Numeric representation of the version */
-#define LIBDE265_NUMERIC_VERSION 0x01001300
+#define LIBDE265_NUMERIC_VERSION 0x01001400
 
-#define LIBDE265_VERSION "1.0.13"
+#define LIBDE265_VERSION "1.0.14"
 
 #endif
