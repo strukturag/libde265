336272b1245c480a4cce8231168e5adebbc1234d
Dirk Farin <dirk.farin@gmail.com>
2024-11-04 17:38:06 +0100

    dllexport: test against _WIN32 and __CYGWIN__

--- libde265/de265.h
+++ libde265/de265.h
@@ -36,7 +36,7 @@ extern "C" {
 #endif
 #include <stdint.h>
 
-#if defined(_WIN32) && !defined(LIBDE265_STATIC_BUILD)
+#if (defined(_WIN32) || defined(__CYGWIN__)) && !defined(LIBDE265_STATIC_BUILD)
   #ifdef LIBDE265_EXPORTS
   #define LIBDE265_API __declspec(dllexport)
   #else
